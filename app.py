44311b8ddac66760453200504473e3399c67e866
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2024-04-11 10:52:06 +0200

    balazs-matyassy: HTML

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Érettségi eredmények |2024|'
+    return render_template('hello_world.html', message='Érettségi eredmények |2024|')
 
 
 @app.route('/akos')
 def akos():
-    return [5, 5, 5, 5, 5]
+    return render_template('eredmenyek.html', name='Ákos', results=[5, 5, 5, 5, 5])
 
 
 @app.route('/balazs')  # Balázs
 def balazs():
-    return [4, 5, 4, 5, 4]
+    return render_template('eredmenyek.html', name='Balázs', results=[4, 5, 4, 5, 4])
 
 
 if __name__ == '__main__':
