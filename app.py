4acdf745a42a3ca1f1e85049a9c3c41bca8fff9f
Akos_Bolyai_acc <103302936+Akos-05-2@users.noreply.github.com>
2024-04-11 08:50:14 +0200

    Add def akos

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Érettségi eredmények |2024|'
 
+@app.route('/akos')
+def akos(): 
+    return 'Ákos'
+
 
 if __name__ == '__main__':
     app.run()
