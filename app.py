6bdafab3948076b58e3467cfb3850291f37a3f0e
Akos_Bolyai_acc <103302936+Akos-05-2@users.noreply.github.com>
2024-04-11 08:35:31 +0200

    Update def hello_world

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Érettségi eredmények 2024'
+    return 'Érettségi eredmények (2024)'
 
 
 if __name__ == '__main__':
